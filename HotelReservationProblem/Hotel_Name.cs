2ee587b9d3da48f2531940e4fedd5f78d693094e
Satantra Tewari <satantra1508@gmail.com>
2020-11-28 11:18:48 +0530

    [satantra] stored the names of the hotels in an enumerator

---
+++ HotelReservationProblem/Hotel_Name.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationProblem
+{
+    public enum HotelName
+    {
+        // using enumerator to store names of the hotels
+        LAKEWOOD,
+        BRIDGEWOOD,
+        RIDGEWOOD
+    }
+}
