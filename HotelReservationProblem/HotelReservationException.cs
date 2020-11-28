ebc9b11ed09a1766ef1b7a632e6e56ccaa05c046
Satantra Tewari <satantra1508@gmail.com>
2020-11-28 11:21:50 +0530

    [satantra] added a class to handle Custom Exceptions

---
+++ HotelReservationProblem/HotelReservationException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationProblem
+{
+    public class HotelReservationException:Exception
+    {
+        // storing different types of expected exception in the enum
+        public enum ExceptionType
+        {
+            INVALID_CUSTOMER_NAME,
+            INVALID_CUSTOMER_TYPE,
+            INVALID_DATES
+        }
+        public ExceptionType type;
+        public HotelReservationException(ExceptionType type, string message):base(message)
+        {
+            this.type = type;
+        }
+    }
+}
