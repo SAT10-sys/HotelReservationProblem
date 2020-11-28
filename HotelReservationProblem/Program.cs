e7969ac9c23153d7e3fd8194c3a54082427dfbfa
Satantra Tewari <satantra1508@gmail.com>
2020-11-28 11:16:08 +0530

    [satantra] displayed welcome message

---
+++ HotelReservationProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HotelReservationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Hotel Reservation Program ");
+        }
+    }
+}
